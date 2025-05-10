0ec6c1d18567d1775a50693feb2237b51faaf308
Seungmin Choi <seungmin960@gmail.com>
2025-05-10 21:45:28 +0900

    one field dto update

--- src/main/kotlin/com/example/skystWaffleunivServer/dto/SongRequestDto.kt
+++ src/main/kotlin/com/example/skystWaffleunivServer/dto/SongRequestDto.kt
@@ -2,10 +2,20 @@ package com.example.skystWaffleunivServer.dto
 
 import com.example.skystWaffleunivServer.domain.SongRequestEntity
 
-class SongRequestDto() {
+class SongRequestDto(
+    val id: Long? = null,
+    var title: String,
+    var artist: String,
+    var sourceUrl: String,
+) {
     companion object {
         fun fromEntity(entity: SongRequestEntity): SongRequestDto {
-            return SongRequestDto()
+            return SongRequestDto(
+                id = entity.id,
+                title = entity.title,
+                artist = entity.artist,
+                sourceUrl = entity.sourceUrl,
+            )
         }
     }
 }
--- src/main/kotlin/com/example/skystWaffleunivServer/sock/controller/SocketController.kt
+++ src/main/kotlin/com/example/skystWaffleunivServer/sock/controller/SocketController.kt
@@ -4,9 +4,9 @@ import com.example.skystWaffleunivServer.dto.SongRequestDto
 import com.example.skystWaffleunivServer.service.RoomService
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.simp.SimpMessagingTemplate
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PathVariable
-import java.security.Principal
 
 @Controller
 class SocketController(
@@ -17,10 +17,10 @@ class SocketController(
     fun handleSongRequest(
         @PathVariable roomId: Long,
         songRequestDto: SongRequestDto,
-        principal: Principal,
+        @AuthenticationPrincipal userId: Long,
     ) {
         // Handle the song request
         // For example, you can send the notification to a specific topic
-        roomService.addSongToRoom(principal.name.toLong(), roomId, songRequestDto)
+        roomService.addSongToRoom(userId, roomId, songRequestDto)
     }
 }
